f18600c5523807b988288cf90e2cc3dfd6203291
Neotrax23 <andrikov2018@gmail.com>
2025-05-21 16:44:45 +0300

    добавен клас NonFoodProduct с ценообразуване според валидност и надценка

---
+++ src/NonFoodProduct.java
@@ -0,0 +1,15 @@
+import java.time.LocalDate;
+
+public class NonFoodProduct extends Product {
+    public NonFoodProduct(int id, String name, double deliveryPrice, LocalDate expirationDate, int quantity) {
+        super(id, name, deliveryPrice, expirationDate, quantity);
+    }
+
+    @Override
+    public double calculateSellingPrice(Store store) {
+        if (isExpired()) return 0;
+
+        double basePrice = getDeliveryPrice() * (1 + store.getNonFoodMarkup());
+        return isAboutToExpire(store) ? basePrice * (1 - store.getExpirationDiscount()) : basePrice;
+    }
+}
